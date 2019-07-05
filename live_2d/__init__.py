5882c601a462c99a012d52a671b65709f6e3ea82
Benjamin Barad <barad.benjamin@gene.com>
2019-07-05 13:21:06 -0700

    implemented static pages, webapp.js, and tooltips and updated options pane

--- live_2d/__init__.py
+++ live_2d/__init__.py
@@ -1,9 +1,15 @@
+import os
+
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options, parse_command_line
 from tornado.web import Application, RequestHandler
 
 define('port', default=8181, help='port to listen on')
+
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+}
 
 class IndexHandler(RequestHandler):
     def get(self):
@@ -13,7 +19,8 @@ def main():
     """Construct and serve the tornado app"""
 
     parse_command_line()
-    app=Application([(r"/", IndexHandler)])
+    app=Application([(r"/", IndexHandler),
+        ],**settings)
     app.listen(options.port)
     print('Listening on http://localhost:%i' % options.port)
     IOLoop.current().start()
---
+++ live_2d/static/webapp.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+  $('[rel="popover"]').popover(
+    {container: 'body'}
+  )
+})
+
+
+$(document).ready(function () {
+  $('[rel="tooltip"]').tooltip(
+    {container: 'body'}
+  )
+})
