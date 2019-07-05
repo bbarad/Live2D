daea6f249e7bc731554e8884f24aa443fa802f32
Benjamin Barad <benjamin.barad@gmail.com>
2019-07-05 08:43:19 -0700

    started building tornado webapp

---
+++ live_2d/__init__.py
@@ -0,0 +1,22 @@
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+from tornado.web import Application, RequestHandler
+
+define('port', default=8181, help='port to listen on')
+
+class IndexHandler(RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+def main():
+    """Construct and serve the tornado app"""
+
+    parse_command_line()
+    app=Application([(r"/", IndexHandler)])
+    app.listen(options.port)
+    print('Listening on http://localhost:%i' % options.port)
+    IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'tornado',
+]
+
+setup(
+    name="live_2d_classification",
+    version="0.0",
+    description="Web-interfaced (tornado driven) live 2d classification using warp and cistem",
+    author="Benjamin Barad",
+    author_email="baradb@gene.com",
+    keywords="cryoem warp cistem tornado",
+    packages=find_packages(),
+    instalL_requires=requires,
+    python_requires=">=3.5"
+    entry_points={
+        'console_scripts': [
+            'serve_app = live_2d:main',
+        ]
+    }
+)
