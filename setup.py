c9917c208710331c52cfc7cafd8e18c7a28182a9
Benjamin Barad <barad.benjamin@gene.com>
2019-09-24 09:53:54 -0700

    removed pyem dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
+
+#
+# Copyright 2019 Genentech Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 """ Live2D is a lightweight application for automated generation of 2D classes iteratively during collection of single particle electron microscopy data. It works in concert with Warp on-the-fly processing and uses cisTEM 2's 2d classification utilities.
 
-After installation, run the application with ``live2d``. Server configurations and latest run data can be found in ``~/.live2d/`` (or ``%USERPROFILE%\.live2d`` in windows land)
+After installation, run the application with ``live2d``. Server configurations and latest run data can be found in ``~/.live2d/`` (or ``%USERPROFILE%\.live2d\`` in windows land).
+
+This live2d version is built against warp 1.0.7b3 and cisTEM 2 r1132. Changes to the interfaces of either app may require updating Live2D.
 """
 
 from setuptools import setup, find_packages
@@ -12,7 +31,6 @@ requires = [
     "multiprocess>=0.70.8",
     "numpy>=1.16.2",
     "pandas>=0.24.2",
-    "pyem @ git+https://github.com/bbarad/pyem.git@58fc29e7b27a55d46ac78357b246f37819bfb3ee#egg=pyem",
     "scipy>=1.2.1",
     "terminado>=0.8.1",
     "Sphinx==2.2.0",
